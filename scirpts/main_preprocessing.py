7a6ac03afabe67fadb8d8a04464ee6b7b8a72e72
julesrisse <jules.risse@gmail.com>
2024-07-19 12:42:17 +0200

    added main preprocessing script

---
+++ scirpts/main_preprocessing.py
@@ -0,0 +1,61 @@
+from eopatches_for_sampling import create_all_eopatches
+from creation_patchlets import create_all_patchlets
+from patchlets_to_npz import patchlets_to_npz_files
+from normalization import calculate_normalization_factors
+from create_kfold import k_folds
+from os import getenv
+import logging
+import time
+
+logging.basicConfig(level=logging.INFO)
+NIVA_PROJECT_DATA_ROOT = getenv('NIVA_PROJECT_DATA_ROOT')
+
+
+def main():
+    if not NIVA_PROJECT_DATA_ROOT:
+        raise ValueError(
+            "NIVA_PROJECT_DATA_ROOT environment variable is not set")
+
+    durations = []
+
+    try:
+        start_time = time.time()
+        create_all_eopatches()
+        end_time = time.time()
+        durations.append(('create_all_eopatches', end_time - start_time))
+
+        start_time = time.time()
+        create_all_patchlets()
+        end_time = time.time()
+        durations.append(('create_all_patchlets', end_time - start_time))
+
+        start_time = time.time()
+        patchlets_to_npz_files()
+        end_time = time.time()
+        durations.append(('patchlets_to_npz_files', end_time - start_time))
+
+        start_time = time.time()
+        calculate_normalization_factors()
+        end_time = time.time()
+        durations.append(
+            ('calculate_normalization_factors', end_time - start_time))
+
+        start_time = time.time()
+        k_folds()
+        end_time = time.time()
+        durations.append(('k_folds', end_time - start_time))
+
+        logging.info("\n--- Overview ---")
+        total_time = 0
+        for step, duration in durations:
+            logging.info(f"Total time for {step}: {duration:.2f} seconds")
+            total_time += duration
+        logging.info(f"Total time for all steps: {total_time:.2f} seconds")
+
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
