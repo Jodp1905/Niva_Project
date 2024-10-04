e80261b4285060117cd59aecaf13cf9a1abae0c7
julesrisse <jules.risse@gmail.com>
2024-10-04 11:32:39 +0200

    added guards in tracing_wrapper script and improved parameter descriptions

--- scripts/tracing_wrapper.sh
+++ scripts/tracing_wrapper.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
+# ======================================= PARAMETERS =======================================
+
+# Project virtual environment path
+PYTHON_VENV_PATH="${PROJECT_DIR}/venv-niva"
+
+# Darshan Library Path
+DARSHAN_LIBPATH="${HOME}/software/darshan-3.4.5/darshan-runtime/install/lib/libdarshan.so"
+
+# Output Directories
+NSIGHT_LOGDIR="${HOME}/niva/nsight_logs"
+
+# DARSHAN PARAMETERS
+# Darshan DXT (Darshan eXtended Tracing) generates more detailed I/O traces
+DARSHAN_DXT=1
+
+# NSIGHT PARAMETERS
+# Nsight NVTX (NVIDIA Tools Extension) enables profiling with NVTX annotations
+# Sampled batch training steps from the Python script will be captured
+# nsight_batch_profiling needs to be enabled in yaml configuration file
+NSIGHT_NVTX=1
+# Nsight Storage Metrics enables capturing I/O metrics
+NSIGHT_STORAGE_METRICS=1
+# Path to Lustre LLITE directory for capturing Lustre I/O metrics
+LUSTRE_LLITE_DIR="/mnt/lustre-stats/llite"
+# Nsight Python Sampling enables capturing Python function calls (Higher overhead)
+NSIGHT_PYTHON_SAMPLING=0
+
+# ======================================== SCRIPT ========================================
+
 # Arguments:
 # $1: Tracing tool (either 'darshan' or 'nsight')
 # $2: Command to execute (e.g., python or bash script) and its arguments
 TRACING_TOOL=$1
 shift
 EXEC_COMMAND=$@
-
-# Path Parameters
-PROJECT_DIR=$HOME
-PYTHON_VENV_PATH="${PROJECT_DIR}/venv-niva"
-NSIGHT_PATH="${PROJECT_DIR}/software/nsight_2024.6.0/target-linux-x64"
-LUSTRE_LLITE_DIR="/mnt/lustre-stats/llite"
-DARSHAN_LIBPATH="${PROJECT_DIR}/software/darshan-3.4.5/darshan-runtime/install/lib/libdarshan.so"
-DARSHAN_LOGDIR="${PROJECT_DIR}/niva/darshan_logs"
-NSIGHT_LOGDIR="${PROJECT_DIR}/niva/nsight_logs"
-
-# Tracing Parameters
-ENABLE_TRACING=1
-
-# Darshan DXT (Darshan eXtended Tracing) generates more detailed I/O traces
-DARSHAN_DXT=1
-
-# Nsight NVTX (NVIDIA Tools Extension) enables profiling with NVTX annotations
-# Sampled batch training steps from the Python script will be captured
-# nsight_batch_profiling needs to be enabled in yaml configuration file
-NSIGHT_NVTX=1
 
 # Tracing tool choice check
 if [ "$TRACING_TOOL" == "darshan" ]; then
@@ -36,6 +45,15 @@ else
     echo "Invalid tracing tool: $TRACING_TOOL"
     echo "  tracing_tool: 'darshan' or 'nsight'"
     exit 1
+fi
+
+# logdir setup
+if [ "$TRACING_TOOL" == "darshan" ]; then
+    mkdir -p "${DARSHAN_LOGDIR}"
+    echo "Darshan log directory: ${DARSHAN_LOGDIR}"
+elif [ "$TRACING_TOOL" == "nsight" ]; then
+    mkdir -p "${NSIGHT_LOGDIR}"
+    echo "Nsight log directory: ${NSIGHT_LOGDIR}"
 fi
 
 # Darshan Parameters
@@ -63,18 +81,44 @@ if [ -z "$NIVA_PROJECT_DATA_ROOT" ]; then
 else
     echo "Project data root directory: ${NIVA_PROJECT_DATA_ROOT}"
 fi
-# darshan library path
-if [ "$ENABLE_TRACING" -eq 1 ] && [ "$TRACING_TOOL" == "darshan" ]; then
+
+# Tracing tool specific checks
+# darshan checks
+if [ "$TRACING_TOOL" == "darshan" ]; then
+    # is darshan library path set
+    if [ -z "$DARSHAN_LIBPATH" ]; then
+        echo "Error: DARSHAN_LIBPATH is not set."
+        echo "Please set it to the path of the Darshan library."
+        exit 1
+    fi
+    # is darshan library path valid
     if [ ! -f "$DARSHAN_LIBPATH" ]; then
         echo "Error: DARSHAN_LIBPATH is not a valid file: $DARSHAN_LIBPATH"
         exit 1
     else
         echo "Darshan library found at ${DARSHAN_LIBPATH}"
     fi
+# nsight checks
+else
+    # is the nsys command available
+    if ! command -v nsys &>/dev/null; then
+        echo "Error: Nsight command 'nsys' is not available."
+        echo "Please make sure the Nsight Systems is installed and available in the PATH."
+        exit 1
+    fi
+    # is nsight lustre llite dir set
+    if [ -z "$LUSTR_LLITE_DIR" ]; then
+        echo "Warning: LUSTRE_LLITE_DIR is not set."
+        echo "Lustre I/O metrics will not be captured."
+    fi
+    # is nsight lustre llite dir valid
+    if [ ! -f "$LUSTRE_LLITE_DIR" ]; then
+        echo "Warning: LUSTRE_LLITE_DIR is not a valid directory: $LUSTRE_LLITE_DIR"
+        echo "Lustre I/O metrics will not be captured."
+    fi
 fi
 
 # Setup output directory
-export TZ="Europe/Paris"
 date_str=$(date +%m%d%Y-%H%M)
 if [ ! -z "$SLURM_JOB_ID" ]; then
     suffix="${SLURM_JOB_ID}_${date_str}"
@@ -84,31 +128,28 @@ fi
 run_name="traced_${suffix}"
 
 # Tracing execution logic
-if [ "$ENABLE_TRACING" -eq 1 ]; then
-    # nsight
-    if [ "$TRACING_TOOL" == "nsight" ]; then
-        echo "Nsight log file: ${NSIGHT_LOGDIR}/${run_name}"
-        trace_cmd=$(
-            cat <<EOF
-${NSIGHT_PATH}/nsys profile \
---enable storage_metrics,\
---lustre-volumes=all,\
---lustre-llite-dir="${LUSTRE_LLITE_DIR}" \
---output="${NSIGHT_LOGDIR}/${run_name}" \
---python-sampling=false
-EOF
-        )
-        if [ "$NSIGHT_NVTX" -eq 1 ]; then
-            NVTX_RANGE="BATCH"
-            trace_cmd="${trace_cmd}\
+# nsight
+if [ "$TRACING_TOOL" == "nsight" ]; then
+    echo "Nsight log file: ${NSIGHT_LOGDIR}/${run_name}"
+    trace_cmd="nsys profile --output=${NSIGHT_LOGDIR}/${run_name}"
+    if [ "$NSIGHT_STORAGE_METRICS" -eq 1 ]; then
+        trace_cmd="${trace_cmd}\
+ --enable storage_metrics,--lustre-volumes=all,--lustre-llite-dir=${LUSTRE_LLITE_DIR}"
+    fi
+    if [ "$NSIGHT_PYTHON_SAMPLING" -eq 1 ]; then
+        trace_cmd="${trace_cmd}\
+ --python-sampling=true"
+    fi
+    if [ "$NSIGHT_NVTX" -eq 1 ]; then
+        NVTX_RANGE="BATCH"
+        trace_cmd="${trace_cmd}\
  --capture-range=nvtx\
  --nvtx-capture=${NVTX_RANGE}\
  --capture-range-end=repeat"
-        fi
-    # darshan
-    elif [ "$TRACING_TOOL" == "darshan" ]; then
-        trace_cmd="env LD_PRELOAD=${DARSHAN_LIBPATH}"
     fi
+# darshan
+elif [ "$TRACING_TOOL" == "darshan" ]; then
+    trace_cmd="env LD_PRELOAD=${DARSHAN_LIBPATH}"
 fi
 
 full_cmd="${trace_cmd} ${EXEC_COMMAND}"
